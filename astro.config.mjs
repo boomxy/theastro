180c92c645c7d6ca27565997c481ac2263cf1729
boom <2794865850@qq.com>
2025-04-21 15:59:10 +0800

    解决 GitHub Pages 无法访问 _astro 文件夹下 JS 文件的问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "http://localhost:4321",
-  integrations: [preact()]
+  integrations: [preact()],
+  build: { // 修复 github pages 访问 _astro 404 的问题，最简单的解决方案是在你的项目根目录创建一个名为 .nojekyll 的空文件。这会告诉 GitHub Pages 不要使用 Jekyll 处理你的网站，从而允许访问以下划线开头的文件和目录。
+    assets: 'astro-assets' // 将 _astro 改为 astro-assets
+  }
 });
